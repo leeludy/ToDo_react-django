2bd760c71464d2939e295aaff455ccb093904dd4
sibylle <sibylle.p@protonmail.com>
2023-09-21 20:02:06 +0200

    init tanstak query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import FormTodo from './Tasks/FormAddTask';
 import ListTasks from './Tasks/ListTasks';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  // desable retry on fail
+  defaultOptions: { queries: { retry: false } },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <div className="h-screen w-screen p-4 bg-stone-100">
         <h1>ToDo</h1>
         <section className="bg-white max-w-[500px] w-full m-auto rounded-md shadow-xl p-4 mb-8">
@@ -13,7 +19,7 @@ function App() {
           <ListTasks />
         </section>
       </div>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- frontend/src/Tasks/ListTasks.tsx
+++ frontend/src/Tasks/ListTasks.tsx
@@ -1,37 +1,32 @@
+import { useQuery } from '@tanstack/react-query';
 import Task from './Task';
 
-// test before building queries
-const tasks = [
-  {
-    id: 1,
-    title: 'Muffins',
-    description: 'bake baby !',
-    completed: true,
-  },
-  {
-    id: 2,
-    title: 'Dev',
-    description: 'Codecodecodecode',
-    completed: false,
-  },
-  {
-    id: 3,
-    title: 'Sail',
-    description: 'sea is waiting !',
-    completed: false,
-  },
-];
-
 export default function ListTasks() {
+  const {
+    data: tasks,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['todos'],
+    queryFn: () => getTodos,
+  });
+  console.log('data query todos', tasks);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (error) {
+    return <span>{JSON.stringify(error)}</span>;
+  }
   return (
     <div className="flex flex-col justify-between bg-stone-100 p-4 rounded-lg">
-      {tasks.map((task, index) => (
+      {tasks?.map((task, index) => (
         <Task
           key={task.id}
           {...task}
           /*   onRename={(newName) => renameTask(index, newName)}
       onTrash={() => removeTask(index)}
-      onToggle={(done) => updateTaskDone(index, done)} */
+      onToggle={(done) => updateTaskDone(index, done)}  */
         />
       ))}
     </div>
