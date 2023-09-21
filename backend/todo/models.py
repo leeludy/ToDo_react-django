a65b450969d7729f9fcf79064503332d00be2ca6
sibylle <sibylle.p@protonmail.com>
2023-09-21 22:46:47 +0200

    temporary remove description property

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Todo(models.Model):
     title = models.CharField(max_length=120)
-    description = models.TextField()
+    """ description = models.TextField() """
     completed = models.BooleanField(default=False)
 
     def _str_(self):
